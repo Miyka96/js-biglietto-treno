c56c7508317c0b19dc3efeaf23013b613e9d09d3
Miyka96 <micaelaseminara@outlook.it>
2022-02-06 23:30:25 +0100

    fixed

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const km= prompt ("Inserire il numero di Km da percorrere");
-const age= prompt ("Indicare la propria età");
+const km= parseInt (prompt ("Inserire il numero di Km da percorrere") );
+
+if ( isNaN (km) ) {
+    alert ("Inserisci numeri!");
+}
+
+const age= parseInt (prompt ("Indicare la propria età") );
+
+if ( isNaN (age) ) {
+    alert("Inserisci solo numeri!");
+}
+
 const priceForKm= 0.21;
 let priceCalculated= km * priceForKm;
 let minorPrice= priceCalculated - (priceCalculated * 20) / 100;
 let seniorPrice= priceCalculated - (priceCalculated * 40) / 100;
+
 
 
 if (age < 18) {
@@ -12,10 +23,7 @@ if (age < 18) {
 else if (age > 65) {
     document.getElementById("price").innerHTML= parseFloat (priceCalculated - seniorPrice) .toFixed (2);
 }
+
 else {
     document.getElementById("price").innerHTML= parseFloat (priceCalculated) .toFixed (2);
-}
-
-if (age == isNaN) {
-    alert("Inserisci solo numeri!");
 }
