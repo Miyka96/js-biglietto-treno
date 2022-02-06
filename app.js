90f8ea769489dd68994cae9ad394e46976b4b1a8
Miyka96 <micaelaseminara@outlook.it>
2022-02-06 21:20:50 +0100

    fixed js price calc

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const km= prompt ("Inserire il numero di Km da percorrere");
 const age= prompt ("Indicare la propria età");
 const priceForKm= 0.21;
 let priceCalculated= km * priceForKm;
+let minorPrice= priceCalculated - (priceCalculated * 20) / 100;
+let seniorPrice= priceCalculated - (priceCalculated * 40) / 100;
 
-document.getElementById("price").innerHTML= (priceCalculated);
 
 if (age < 18) {
-    document.getElementById("price").innerHTML= priceCalculated= priceCalculated - (priceCalculated * 20) / 100
+    document.getElementById("price").innerHTML= parseFloat (priceCalculated - minorPrice) .toFixed (2);
 }
 else if (age > 65) {
-    document.getElementById("price").innerHTML= priceCalculated= priceCalculated - (priceCalculated * 40) / 100
+    document.getElementById("price").innerHTML= parseFloat (priceCalculated - seniorPrice) .toFixed (2);
+}
+else {
+    document.getElementById("price").innerHTML= parseFloat (priceCalculated) .toFixed (2);
 }
